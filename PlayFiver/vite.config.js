2158d29a572ab7209228b1e4226e6de9fcefdf7a
vinieves <viniciusgow@hotmail.com>
2025-06-21 17:12:12 -0300

    Feat: Adiciona alias para /public no Vite

--- PlayFiver/vite.config.js
+++ PlayFiver/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 import i18n from "laravel-vue-i18n/vite";
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -18,7 +19,9 @@ export default defineConfig({
         i18n(),
     ],
     resolve: {
-        alias: {},
+        alias: {
+            '/public': path.resolve(__dirname, 'public')
+        },
     },
     build: {
         chunkSizeWarningLimit: 2024,
